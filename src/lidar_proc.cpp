332a41a2d13f9d6ab2820a2bf38bd5d0727f5f53
Stone Boyen <stone@qzhuzi.com>
2019-01-22 16:25:39 +0800

    增加三角函数表等函数，对测据数据做标定

--- src/lidar_proc.cpp
+++ src/lidar_proc.cpp
@@ -1,5 +1,7 @@
 #include "ros/ros.h" 
 #include "std_msgs/String.h"
+#include <math.h>
+#include <angles/angles.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/PCLPointCloud2.h>
@@ -48,12 +50,12 @@ ofstream outfile;
 ofstream outfile_dis_accu;
 //确定目标区域
 float x_roi_l= -0.1;float x_roi_r=0.1;
-float z_roi_d= -0.3;float z_roi_u=0.1;
+float z_roi_d= -2.2;float z_roi_u=0.3;
 float y_roi=4;
 //精度
 float dis_accu = 0.0;
 int frame_accu = 0;
-double  H_accu =5.55;
+double  H_accu =6.341;
 #define PI  3.1415926
 //水平角分辨率
 float v_angle_roi=1.0; //水平角度统计范围
@@ -61,22 +63,42 @@ float x_v_roi=tan(v_angle_roi*PI/180.0)*H_accu;
 float v_resulotion_accu = 0.0;
 int frame_v_resolution = 0;
 
+//
+void InitTables_chaowu();
+static const uint16_t ROTATION_MAX_UNITS    = 36000u;     // [deg/100]
+static const int Num_Of_Detector=64;
+static const float ROTATION_RESOLUTION      =     0.01f;  // [deg]
+float sin_rot_table_[ROTATION_MAX_UNITS];
+float cos_rot_table_[ROTATION_MAX_UNITS];
+
+int thita[Num_Of_Detector];
+float cos_thita_value[Num_Of_Detector];
+float sin_thita_value[Num_Of_Detector];
+
+
 void dis_accuracy(const pcl::PointCloud<LASER_POINT_NEW>::Ptr in_cloud_ptr) //4  //精度
 {
   float x, y,z;
   float dis = 0.0;
+  float D_corr = 0.0;
+  float dis_lidar = 0.0;
+  int laser_number=0;
+  int alpha=0;
   int flag = 0;
   for (unsigned int i=0; i<in_cloud_ptr->points.size(); i++)
   {
     x = in_cloud_ptr->points[i].x;
     y = in_cloud_ptr->points[i].y;
     z = in_cloud_ptr->points[i].z;
-    if(x<x_roi_r && x>x_roi_l && z<z_roi_u && z>z_roi_d && y>H_accu-2)
+    if(x<x_roi_r && x>x_roi_l && z<z_roi_u && z>z_roi_d && y>0.3)
     {
       dis = dis + y;
       flag++;
-      //  ROS_INFO("ID:%d,  y:%f", in_cloud_ptr->points[i].laserid,y);
-      outfile<<"ID:"<<in_cloud_ptr->points[i].laserid<<"; dis:"<<in_cloud_ptr->points[i].range<<"; y:"<<y<<"; bis:"<<H_accu-y<<"; Intensity:"<<in_cloud_ptr->points[i].intensity<<"; h_angle:"<<in_cloud_ptr->points[i].h_angle<<endl;
+      laser_number=  in_cloud_ptr->points[i].laserid;
+      dis_lidar=in_cloud_ptr->points[i].range;
+      alpha=in_cloud_ptr->points[i].h_angle*100;
+      D_corr=H_accu/cos_rot_table_[alpha]/cos_thita_value[laser_number]-dis_lidar;
+      outfile<<"ID:"<<laser_number<<"; dis:"<<dis_lidar<<"; y:"<<y<<"; bis:"<<H_accu-y<<"; Intensity:"<<in_cloud_ptr->points[i].intensity<<"; h_angle:"<<alpha<<"; D_corr:"<<D_corr<<endl;
     }
   }
   if(flag>=10)
@@ -153,6 +175,7 @@ int main(int argc, char **argv)
 { 
   outfile.open("data.txt");
   outfile_dis_accu.open("dis_accu.txt");
+  InitTables_chaowu();
   ros::Time::init();
   ros::init(argc, argv, "lidar_proc_node");
   ros::NodeHandle h;
@@ -165,3 +188,71 @@ int main(int argc, char **argv)
   ros::spin();
   return 0;
 }  
+
+void InitTables_chaowu()
+{
+
+  /////  l209
+  for (uint16_t rot_index = 0; rot_index < ROTATION_MAX_UNITS; ++rot_index)
+  {
+    float rotation = angles::from_degrees(ROTATION_RESOLUTION * rot_index);
+    cos_rot_table_[rot_index] = cosf(rotation);
+    sin_rot_table_[rot_index] = sinf(rotation);
+  }
+
+  memset(thita,0,sizeof(int)*Num_Of_Detector);
+  memset(cos_thita_value,0,sizeof(float)*Num_Of_Detector);
+  memset(sin_thita_value,0,sizeof(float)*Num_Of_Detector);
+
+
+  int start=-940;//245-- -940
+  for (int i=0;i<16;++i)
+  {
+    thita[i]=start+i*79;
+  }
+
+  start=-268;//285-- -268
+  for (int i=0;i<8;++i)
+  {
+    thita[i+16]=start+i*79;
+  }
+  start=-900;// -347--- -900
+  for (int i=0;i<8;++i)
+  {
+    thita[i+24]=start+i*79;
+  }
+
+  start=-2209;// -1024--- -2209
+  for (int i=0;i<16;++i)
+  {
+    thita[i+32]=start+i*79;
+  }
+
+  start=-1538;// -1538--- -985
+  for (int i=0;i<8;++i)
+  {
+    thita[i+48]=start+i*79;
+  }
+  start=-2170;// -2170--- -1617
+  for (int i=0;i<8;++i)
+  {
+    thita[i+56]=start+i*79;
+  }
+
+
+
+  for (int i=0;i<Num_Of_Detector;++i)
+  {
+    if (thita[i]<0)
+    {
+      cos_thita_value[i]=cos_rot_table_[-thita[i]];
+      sin_thita_value[i]=-sin_rot_table_[-thita[i]];
+    }
+    else
+    {
+      cos_thita_value[i]=cos_rot_table_[thita[i]];
+      sin_thita_value[i]=sin_rot_table_[thita[i]];
+    }
+  }
+
+}
